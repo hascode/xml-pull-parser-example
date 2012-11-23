07add5a7b2069550a69d19ebfd95b37137eec37c
Micha Kops <micha.kops@gmail.com>
2012-11-23 22:43:11 +0100

    Initial import.

---
+++ README.md
@@ -0,0 +1,9 @@
+# XML Pull Parser Example
+
+A simple example how to parse some XML from the public Atlassian JIRA instance using the XML Pull Parser API.
+
+Please feel free to take a look at [my blog] for some more snippets.
+
+---
+
+**2012 Micha Kops / hasCode.com**
---
+++ src/main/java/com/hascode/tutorial/ParserExample.java
@@ -0,0 +1,39 @@
+package com.hascode.tutorial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+public class ParserExample {
+	private static final String urlString = "https://jira.atlassian.com/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=&tempMax=200";
+	private static final String ITEM_TITLE = "title";
+	private static final String ITEM_LINK = "link";
+
+	public static void main(final String[] args) throws XmlPullParserException,
+			MalformedURLException, IOException {
+		XmlPullParser xpp = XmlPullParserFactory.newInstance().newPullParser();
+		URL url = new URL(urlString);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				url.openStream()));
+		try {
+			xpp.setInput(in);
+			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+				xpp.next();
+				if (ITEM_TITLE.equals(xpp.getName())) {
+					System.out.print("Issue: " + xpp.nextText());
+				}
+				if (ITEM_LINK.equals(xpp.getName())) {
+					System.out.print(" at " + xpp.nextText() + "\n");
+				}
+			}
+		} finally {
+			in.close();
+		}
+	}
+}
